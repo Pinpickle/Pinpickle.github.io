98b8402649cf29fedcb824dcbfa92b4850e8b2fe
Christian Silver <christian@decode.org.nz>
2014-11-29 09:58:27 +1300

    Squash deploy bugs
    Add node_modules to excluded folders
    Deploy from _site

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var deploy = require('gulp-gh-pages');
 
 gulp.task('deploy', function () {
-    return gulp.src('./dist/**/*')
+    return gulp.src('_site/**/*')
         .pipe(deploy({branch: "master"}));
 });
