b4fa4e2deeb805ebeb12b88e8a87605bf730cbe5
Christian Silver <christian@decode.org.nz>
2014-12-09 20:42:57 +1100

    Add Google Analytics, Touch video

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,2 +1,10 @@
-//= require jquery/dist/jquery
-var hi = true;
+//= require jquery/dist/jquery.min
+$(document).ready(function() {
+    var resize = function () {
+        $('.video-big').each(function() {
+            $(this).height($(this).width() / 16 * 9);
+        });
+    };
+    resize();
+    $(window).resize(resize);
+});
---
+++ _projects/touch.md
@@ -0,0 +1,6 @@
+---
+title: Touch
+date: 2014-01-20
+namespace: touch
+---
+<iframe class="video-big" src="//www.youtube.com/embed/_RLokNRpz_4" frameborder="0" allowfullscreen></iframe>
