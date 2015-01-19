f1113d0e06ba4f12e1db76f662b42eb73ca57ad8
Christian Silver <christian@decode.org.nz>
2015-01-20 09:58:03 +1300

    Optimise parallax

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -1,4 +1,37 @@
 //= require jquery/dist/jquery.min
+
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+
+// MIT license
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+
 var Pinpickle = function() {
     var self = this;
 
@@ -35,14 +68,16 @@ var Pinpickle = function() {
         };
 
     $window.scroll(function() {
-        didScroll = true;
+        if (!didScroll) {
+            didScroll = true;
+            scrollTimer = requestAnimationFrame(scrollFunction);
+        }
     });
 
     $window.resize(function() {
         didResize = true;
     });
 
-    scrollTimer = setInterval(scrollFunction, 10);
     resizeTimer = setInterval(resizeFunction, 200);
 
     this.addScroll = function(fun, exec) {
